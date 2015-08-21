781ccb5e83fca448fdbb9fd103e5df44af9a7943
Charles Sprayberry <cspray@gmail.com>
2015-08-21 18:29:23 -0400

    refactor property storage

--- src/Cspray/Adhoc/Property.php
+++ src/Cspray/Adhoc/Property.php
@@ -15,33 +15,47 @@ trait Property {
     private $_adhocProperties = [];
 
     public function adhocGetter(string $property, $value) {
-        $this->_adhocProperties[$property . '_getter'] = $value;
+        $this->_adhocEnsurePropertyHasStore($property);
+        $this->_adhocProperties[$property]['get'] = $value;
     }
 
     public function adhocSetter(string $property, callable $callback) {
-        $this->_adhocProperties[$property . '_setter'] = $callback;
+        $this->_adhocEnsurePropertyHasStore($property);
+        $this->_adhocProperties[$property]['set'] = $callback;
+    }
+
+    private function _adhocEnsurePropertyHasStore(string $property) {
+        if (!isset($this->_adhocProperties[$property])) {
+            $this->_adhocProperties[$property] = [];
+        }
+    }
+
+    private function _adhocPropertyHasGetter(string $property) {
+        return isset($this->_adhocProperties[$property]) && isset($this->_adhocProperties[$property]['get']);
+    }
+
+    private function _adhocPropertyHasSetter(string $property) {
+        return isset($this->_adhocProperties[$property]) && isset($this->_adhocProperties[$property]['set']);
     }
 
     public function __get(string $property) {
-        $key = $property . '_getter';
-        if (!isset($this->_adhocProperties[$key])) {
+        if (!$this->_adhocPropertyHasGetter($property)) {
             $msg = 'There is no getter defined for "%s"';
             throw new InvalidPropertyException(sprintf($msg, $property));
         }
 
-        $val = $this->_adhocProperties[$key];
+        $val = $this->_adhocProperties[$property]['get'];
 
         return ($val instanceof \Closure) ? $val->call($this) : $val;
     }
 
     public function __set(string $property, $val) {
-        $key = $property . '_setter';
-        if (!isset($this->_adhocProperties[$key])) {
+        if (!$this->_adhocPropertyHasSetter($property)) {
             $msg = 'There is no setter defined for "%s"';
             throw new InvalidPropertyException(sprintf($msg, $property));
         }
 
-        $this->_adhocProperties[$key]($val);
+        $this->_adhocProperties[$property]['set']->call($this, $val);
     }
 
 }
